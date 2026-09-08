6b96198b98005283ba06b9cd725144c8bce12b40
Izzulfahmy <izzuley123@gmail.com>
2026-09-09 05:17:38 +0700

    n384

--- soal.js
+++ soal.js
@@ -1,43 +1,52 @@
 const questionBank = [
   { 
     image: "soal/1.png",
-    text: "Ayah menanam 5 baris pohon. Setiap baris ada 4 pohon. Berapa jumlah seluruh pohon?", 
-    ans: 20 
-  },
-  { 
-    text: "Ibu membeli 3 kantong jeruk. Setiap kantong berisi 8 jeruk. Berapa jumlah seluruh jeruk Ibu?", 
-    ans: 24 
-  },
-  { 
-    text: "Ada 6 meja di kelas. Setiap meja ada 5 buku. Berapa jumlah seluruh buku di kelas?", 
+    text: "Pak Budi memiliki 5 keranjang kopi khas Jember. Setiap keranjang berisi 6 bungkus kopi. Berapa jumlah bungkus kopi seluruhnya?", 
     ans: 30 
   },
   { 
-    text: "Budi mempunyai 4 kotak pensil. Tiap kotak berisi 6 pensil. Berapa total pensil Budi?", 
-    ans: 24 
+    image: "soal/2.png",
+    text: "Petani di Jember memanen 6 keranjang edamame. Setiap keranjang berisi 8 ikat edamame. Berapa jumlah ikat edamame seluruhnya?", 
+    ans: 48 
   },
   { 
-    text: "Lani membeli 5 bungkus kue. Setiap bungkus berisi 6 kue. Berapa jumlah seluruh kue Lani?", 
-    ans: 30 
+    image: "soal/3.png",
+    text: "Ibu membuat 7 kotak tape untuk oleh-oleh. Setiap kotak berisi 9 potong tape. Berapa jumlah potongan tape seluruhnya?", 
+    ans: 63 
   },
   { 
-    text: "Di halaman terdapat 7 sepeda. Setiap sepeda memiliki 2 roda. Berapa jumlah seluruh roda?", 
-    ans: 14 
+    image: "soal/4.png",
+    text: "Sebuah toko oleh-oleh memiliki 2 nampan suwar-suwir. Setiap nampan berisi 11 buah suwar-suwir. Berapa jumlah suwar-suwir seluruhnya?", 
+    ans: 22 
   },
   { 
-    text: "Siti membeli 4 piring donat. Setiap piring berisi 5 donat. Berapa jumlah seluruh donat?", 
-    ans: 20 
+    image: "soal/5.png",
+    text: "Pengrajin batik membuat 3 kain batik Jember. Pada setiap kain terdapat 12 motif batik. Berapa jumlah motif batik yang dibuat seluruhnya?", 
+    ans: 36 
   },
   { 
-    text: "Toko memiliki 8 kardus mi. Setiap kardus berisi 5 bungkus. Berapa jumlah bungkus mi?", 
+    image: "soal/6.png",
+    text: "Seorang petani memiliki 4 keranjang daun tembakau. Setiap keranjang berisi 10 lembar daun tembakau. Berapa jumlah daun tembakau seluruhnya?", 
     ans: 40 
   },
   { 
-    text: "Eko membeli 3 kotak kelereng. Setiap kotak berisi 9 kelereng. Berapa total kelereng Eko?", 
-    ans: 27 
+    image: "soal/7.png",
+    text: "Sebuah toko oleh-oleh menyiapkan 4 rak. Setiap rak berisi 12 bungkus makanan khas Jember. Berapa jumlah bungkus makanan yang ada di seluruh rak?", 
+    ans: 48 
   },
   { 
-    text: "Petani memanen 6 keranjang apel. Setiap keranjang berisi 6 apel. Berapa jumlah seluruh apel?", 
-    ans: 36 
+    image: "soal/8.png",
+    text: "Dalam sebuah pertunjukan budaya Jember, terdapat 2 kelompok penari. Setiap kelompok terdiri dari 13 penari. Berapa jumlah penari yang mengikuti pertunjukan tersebut?", 
+    ans: 26 
+  },
+  { 
+    image: "soal/9.png",
+    text: "Pada kegiatan festival budaya di Jember, panitia menyiapkan 4 meja. Setiap meja memiliki 7 hiasan khas Jember. Berapa jumlah hiasan yang disiapkan panitia seluruhnya?", 
+    ans: 28 
+  },
+  { 
+    image: "soal/10.png",
+    text: "Di sebuah kebun kopi terdapat 3 baris tanaman kopi. Setiap baris memiliki 9 tanaman kopi. Berapa jumlah tanaman kopi seluruhnya?", 
+    ans: 27 
   }
 ];
